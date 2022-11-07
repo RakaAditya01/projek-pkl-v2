4223fb1374c744b755c10e94156a2cd4e98058b0
RakaAaditya01 <raka27275@gmail.com>
2022-11-07 09:33:22 +0700

    Perbaikan Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->date('expired_at')->nullable();
-            $table->enum('role',['admin', 'mahasiswa'])->default('mahasiswa');
             $table->rememberToken();
             $table->timestamps();
         });
