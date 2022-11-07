18d5a1bd513d6779aba5864eb4b487b920597a9a
RakaAaditya01 <raka27275@gmail.com>
2022-11-07 09:38:48 +0700

    perbaikan

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('nim');
             $table->string('name');
-            $table->string('nim');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
