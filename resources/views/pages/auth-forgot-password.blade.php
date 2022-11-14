df9b4eae7c5099a10611070168c03b9de9678335
ahmadhilmy123 <tomohilmy@gmail.com>
2022-11-14 09:35:57 +0700

    forgot pw

--- resources/views/pages/auth-forgot-password.blade.php
+++ resources/views/pages/auth-forgot-password.blade.php
@@ -4,6 +4,8 @@
 
 @push('style')
     <!-- CSS Libraries -->
+    <link rel="stylesheet"
+        href="{{ asset('library/selectric/public/selectric.css') }}">
 @endpush
 
 @section('main')
@@ -12,18 +14,25 @@
             <h4>Forgot Password</h4>
         </div>
 
+        @if (Session::has('message'))
+        <div class="alert alert-success" role="alert">
+           {{ Session::get('message') }}
+       </div>
+   @endif
+
+     <form action="{{ route('forget.password.post') }}" method="POST">
+         @csrf
+
         <div class="card-body">
             <p class="text-muted">We will send a link to reset your password</p>
-            <form method="POST">
+            <form action="{{ route('forget.password.post') }}" method="POST">
+                @csrf
                 <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email"
-                        type="email"
-                        class="form-control"
-                        name="email"
-                        tabindex="1"
-                        required
-                        autofocus>
+                    <label for="email_address">Email</label>
+                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+                                  @if ($errors->has('email'))
+                                      <span class="text-danger">{{ $errors->first('email') }}</span>
+                                  @endif
                 </div>
 
                 <div class="form-group">
--- resources/views/pages/auth-login2.blade.php
+++ resources/views/pages/auth-login2.blade.php
@@ -89,7 +89,7 @@
                             </div>
 
                             <div class="form-group text-right">
-                                <a href="auth-forgot-password.html"
+                                <a href="/auth-forgot-password"
                                     class="float-left mt-3">
                                     Forgot Password?
                                 </a>
