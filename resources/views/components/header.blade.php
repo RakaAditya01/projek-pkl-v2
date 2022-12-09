d43c117f9e5e0819e9fe65ad1929b6f90ad16d40
ARIQFADHLURRAHMAN <fadhlurrahmanariq51@gmail.com>
2022-12-09 08:10:13 -0800

    alert

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -175,69 +175,33 @@
                 </div>
             </div>
         </li>
-        <li class="dropdown dropdown-list-toggle"><a href="#"
-                data-toggle="dropdown"
-                class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+        <?php
+            $stock = \DB::select("SELECT * from Barangs where stock < 1");
+        ?>
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg d-flex">
+            <i class="far fa-bell"></i>
+            <div class="rounded-circle bg-success" style="width: 15px!important; height: 15px!important;">
+                <span style="display: flex!important; justify-content: center!important; text-align: center!important; align-items: center; width: 15px!important; height: 15px!important; font-size: 12px;">{{ count($stock) }}</span>
+            </div>
+        </a>
             <div class="dropdown-menu dropdown-list dropdown-menu-right">
-                <div class="dropdown-header">Notifications
-                    <div class="float-right">
-                        <a href="#">Mark All As Read</a>
-                    </div>
-                </div>
+                <div class="dropdown-header">Notifications</div>
                 <div class="dropdown-list-content dropdown-list-icons">
-                    <a href="#"
-                        class="dropdown-item dropdown-item-unread">
-                        <div class="dropdown-item-icon bg-primary text-white">
-                            <i class="fas fa-code"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            Template update is available now!
-                            <div class="time text-primary">2 Min Ago</div>
-                        </div>
-                    </a>
-                    <a href="#"
-                        class="dropdown-item">
-                        <div class="dropdown-item-icon bg-info text-white">
-                            <i class="far fa-user"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>You</b> and <b>Dedik Sugiharto</b> are now friends
-                            <div class="time">10 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#"
-                        class="dropdown-item">
-                        <div class="dropdown-item-icon bg-success text-white">
-                            <i class="fas fa-check"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
-                            <div class="time">12 Hours Ago</div>
-                        </div>
-                    </a>
+                    @foreach ($stock as $stk)
                     <a href="#"
                         class="dropdown-item">
                         <div class="dropdown-item-icon bg-danger text-white">
                             <i class="fas fa-exclamation-triangle"></i>
                         </div>
                         <div class="dropdown-item-desc">
-                            Low disk space. Let's clean it!
-                            <div class="time">17 Hours Ago</div>
+                            <p>{{ $stk->nama_barang}}</p>
+                            <div class="time">Barang sudah habis</div>
                         </div>
                     </a>
-                    <a href="#"
-                        class="dropdown-item">
-                        <div class="dropdown-item-icon bg-info text-white">
-                            <i class="fas fa-bell"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            Welcome to Stisla template!
-                            <div class="time">Yesterday</div>
-                        </div>
-                    </a>
+                    @endforeach
                 </div>
                 <div class="dropdown-footer text-center">
-                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                    <a href="/barang">Periksa Barang<i class="fas fa-chevron-right"></i></a>
                 </div>
             </div>
         </li>
